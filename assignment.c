230219429f67b12096020d02206836a2e1ceabee
w3iji3 <57030893+w3iji3@users.noreply.github.com>
2021-10-23 21:38:01 +0800

    File input

--- assignment.c
+++ assignment.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() 
 {
-	printf("This is the start of our CS180 Project!\n");
-	printf("Test Push in Discord!\n");
+	FILE* filepath;
+	char filename[100] = "";
+	printf("Please enter the Full Path of the file: \n");
+	scanf_s("%s", &filename, (unsigned int)sizeof(filename));
+	errno_t err = fopen_s(&filepath, filename, "r");
+	if (filepath == NULL)
+	{
+		printf("%s " "File not found.", filename);
+		exit(1);
+	}
 }
