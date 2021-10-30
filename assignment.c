7644e452b05a12529319dd7bd9ba9c12c0086fb9
w3iji3 <57030893+w3iji3@users.noreply.github.com>
2021-10-30 19:43:13 +0800

    cleanup and added function level comments

--- assignment.c
+++ assignment.c
@@ -1,13 +1,44 @@
-#define _CRT_SECURE_NO_WARNINGS
+/*!*****************************************************************************
+\file assignment.c
+\author Tan Wei Jie, Heng Zeng Le, Tan Chor Yeong, Jiang Chuqiao
+\par DP email: weijie.tan\@digipen.edu, h.zengle\@digipen.edu, 
+                choryeong.tan\@digipen.edu, c.jiang\@digipen.edu
+\par Course: CS180/CSD2180
+\par Project Assignment 1
+\date 10-30-2021
+\brief
+This file contains functions that performs the logic for the proposed
+RRR algorithm
+*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 
+/*!*****************************************************************************
+\brief
+    This function inserts an integer to the end of an array
+\param queue
+    The array to insert into
+\param end
+    A variable that keeps track of the current end index of the array
+\param num
+    An integer value to insert into the array
+*******************************************************************************/
 void queueInsert(int* queue, int* end, int num)
 {
     queue[*end] = num;
     ++(*end);
 }
 
+/*!*****************************************************************************
+\brief
+    This function inserts an integer to the start of an array
+\param queue
+    The array to insert into
+\param end
+    A variable that keeps track of the current end index of the array
+\param num
+    An integer value to insert into the array
+*******************************************************************************/
 void queuePushFront(int* queue, int* end, int num)
 {
     ++(*end);
@@ -16,6 +47,17 @@ void queuePushFront(int* queue, int* end, int num)
     queue[0] = num;
 }
 
+/*!*****************************************************************************
+\brief
+    This function "pop off" the value at the front of the array and 
+    moves the subsequent values one position forward
+\param queue
+    The array to "pop" the value from
+\param end
+    A variable that keeps track of the current end index of the array
+\return
+    Returns the value at the front of the array
+*******************************************************************************/
 int queueTop(int* queue, int* end)
 {
     if (*end > 0)
@@ -29,105 +71,20 @@ int queueTop(int* queue, int* end)
     return -1;
 }
 
-void SRTF(int* arrivalTime, int* burstTime, int* priorityLevel, int _numOfProcesseses)
-{
-    int temp[200] = { 0 }, time, count = 0, shortest, numofProcesses = _numOfProcesseses;
-    double turnaboutTime = 0.0, avgWaitingTime = 0.0, end;
-    for (int i = 0; burstTime[i] != '0'; ++i)
-    {
-        temp[i] = burstTime[i];
-        numofProcesses = i + 1;
-    }
-    for (time = 0; count != numofProcesses; time++)
-    {
-        shortest = numofProcesses;
-        for (int i = 0; i < numofProcesses; i++)
-        {
-            if (arrivalTime[i] <= time && burstTime[i] < burstTime[shortest] && burstTime[i]>0)
-                shortest = i;
-        }
-        burstTime[shortest]--;
-        if (burstTime[shortest] == 0)
-        {
-            count++;
-            end = (double)time + 1;
-            avgWaitingTime = avgWaitingTime + end - arrivalTime[shortest] - temp[shortest];
-            turnaboutTime = turnaboutTime + end - arrivalTime[shortest];
-        }
-    }
-    printf("\n\nAverage waiting time = %lf\n", avgWaitingTime / numofProcesses);
-    printf("Average Turnaround time = %lf\n", turnaboutTime / numofProcesses);
-}
-
-void RRAlgorithm(int* _arrivalTime, int* _burstTime, int* _priorityLevel, int _numOfProcesses)
-{
-    int timeQuantum = 0, remainingProcesses = _numOfProcesses, end = 0, index = 0, time = 0,
-        waitTime[200] = { 0 }, remainBurstTime[200] = { 0 }, queue[200] = { 0 }, arrivalflag[200] = { 0 };
-
-    float maxTurnaroundTime = 0.0f, avgTurnaroundTime = 0.0f, maxWaitingTime = 0.0f, avgWaitingTime = 0.0f;
-
-    printf("Please enter Time Quantum: ");
-    scanf("%d", &timeQuantum);
-
-    for (int i = 0; i < _numOfProcesses; ++i)
-        remainBurstTime[i] = _burstTime[i];
-
-    queueInsert(queue, &end, 0);
-    arrivalflag[0] = 1;
-
-    while (remainingProcesses != 0)
-    {
-        index = queueTop(queue, &end);
-
-        if (index > -1 && remainBurstTime[index] <= timeQuantum)
-        {
-            time += remainBurstTime[index];
-            remainBurstTime[index] = 0;
-            waitTime[index] = time - _arrivalTime[index] - _burstTime[index];
-            --remainingProcesses;
-        }
-        else if(index > -1 && remainBurstTime[index] > timeQuantum)
-        {
-            remainBurstTime[index] -= timeQuantum;
-            time += timeQuantum;
-        }
-
-        for (int i = 0; i < _numOfProcesses; ++i)
-        {
-            if (arrivalflag[i] == 0 && _arrivalTime[i] <= time)
-            {
-                queueInsert(queue, &end, i);
-                arrivalflag[i] = 1;
-            }
-        }
-
-        if (index > -1 && remainBurstTime[index] != 0)
-        {
-            queueInsert(queue, &end, index);
-        }
-
-        if (end <= 0)
-        {
-            ++time;
-        }
-    }
-
-    for (int i = 0; i < _numOfProcesses; ++i)
-    {
-        maxWaitingTime += waitTime[i];
-        maxTurnaroundTime += _burstTime[i] + waitTime[i];
-    }
-
-    avgWaitingTime = maxWaitingTime / _numOfProcesses;
-    avgTurnaroundTime = maxTurnaroundTime / _numOfProcesses;
-
-    printf("average turnaround time: %.2f\n", avgTurnaroundTime);
-    printf("maximum turnaround time: %.2f\n", maxTurnaroundTime);
-    printf("average waiting time: %.2f\n", avgWaitingTime);
-    printf("maximum waiting time: %.2f\n", maxWaitingTime);
-}
-
-void RRRF(int* _arrivalTime, int* _burstTime, int* _priorityLevel, int _numOfProcesses)
+/*!*****************************************************************************
+\brief
+    This function contains the logic of the RRR algorithm based off of the
+    principles of the Round Robin Scheduling algorithm
+\param _arrivalTime
+    The array that contains the arrival time of all the processes
+\param _burstTime
+    The array that contains the burst time of all the processes
+\param _priorityLevel
+    The array that contains the priority level of all the processes
+\param _numOfProcesses
+    An integer specifying the total number of processes
+*******************************************************************************/
+void RRR(int* _arrivalTime, int* _burstTime, int* _priorityLevel, int _numOfProcesses)
 {
     int timeQuantum = 0, remainingProcesses = _numOfProcesses, end = 0, index = 0, time = 0, pivotPoint = -1,
         waitTime[200] = { 0 }, remainBurstTime[200] = { 0 }, queue[200] = { 0 }, arrivalflag[200] = { 0 };
@@ -204,13 +161,21 @@ void RRRF(int* _arrivalTime, int* _burstTime, int* _priorityLevel, int _numOfPro
     printf("maximum waiting time: %.2f\n", maxWaitingTime);
 }
 
+/*!*****************************************************************************
+\brief
+    This function opens a file specified through a command line input
+\param argv
+    This is the name of the file to be opened
+\return 
+    Returns the file to be opened
+*******************************************************************************/
 FILE* fileOpen(char** argv)
 {
 	FILE* filepath;
 	filepath = fopen(argv[1], "r");
 	if (filepath == NULL)
 	{
-		printf("%s " "File not found.", argv[1]);
+		printf("%s " "File not found.\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
     return filepath;
@@ -230,9 +195,7 @@ int main(int argc, char** argv)
     arrivalTime[i] = burstTime[i] = priorityLevel[i] = '0';
     numOfProcesses = i;
    
-    RRAlgorithm(arrivalTime, burstTime, priorityLevel, numOfProcesses);
-    RRRF(arrivalTime, burstTime, priorityLevel, numOfProcesses);
-    //SRTF(arrivalTime, burstTime, priorityLevel, numOfProcesses);
+    RRR(arrivalTime, burstTime, priorityLevel, numOfProcesses);
 
     return 0;
 }
